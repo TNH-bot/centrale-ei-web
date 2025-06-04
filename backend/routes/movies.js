3214e0a4863b595dadd71e5436a03d7045018b99
Titouan <titouannguyenhuynh@MacBook-Pro-de-Titouan.local>
2025-06-04 13:54:15 +0200

    get trending and single from TMDB

--- backend/routes/movies.js
+++ backend/routes/movies.js
@@ -7,7 +7,6 @@ import Movie from '../entities/movie.js';
 const router = express.Router();
 
 router.get('/tmdb', function (req, res) {
-  var results = [];
   const options = {
     method: 'GET',
     url: 'https://api.themoviedb.org/3/movie/top_rated',
@@ -22,16 +21,71 @@ router.get('/tmdb', function (req, res) {
   axios
     .request(options)
     .then((axiosres) => {
-      console.log(axiosres.data.results);
-      results = axiosres.data.results;
-      //setMovies(axiosres.data.results);
+      //console.log(axiosres.data.results);
+      console.log('Trending movies fetched from TMDB');
+      res.json({
+        message: 'Trending movies fetched',
+        results: axiosres.data.results,
+      });
     })
     .catch((err) => console.error(err));
-  console.log('HelloWorld');
-  res.json({
-    message: 'Trending movies fetched',
-    results: results,
-  });
+});
+
+router.post('/tmdb/:movieId', function (req, res) {
+  const options = {
+    method: 'GET',
+    url: 'https://api.themoviedb.org/3/movie/' + req.params.movieId,
+    params: { language: 'en-US', page: '1' },
+    headers: {
+      accept: 'application/json',
+      Authorization:
+        'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxZjlmNjAwMzY4MzMzODNkNGIwYjNhNzJiODA3MzdjNCIsInN1YiI6IjY0NzA5YmE4YzVhZGE1MDBkZWU2ZTMxMiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Em7Y9fSW94J91rbuKFjDWxmpWaQzTitxRKNdQ5Lh2Eo',
+    },
+  };
+
+  axios
+    .request(options)
+    .then((axiosres) => {
+      console.log(axiosres.data.results);
+      console.log('Trending movies fetched from TMDB');
+      const movieRepository = appDataSource.getRepository(Movie);
+      console.log(req.body);
+      const newMovie = movieRepository.create({
+        title: req.body.title,
+        release_date: req.body.release_date,
+        poster_path: req.body.poster_path,
+      });
+      /*movieRepository
+        .insert(newMovie)
+        .then(function (savedMovie) {
+          res.status(201).json({
+            message: 'Movie successfully created',
+            id: savedMovie.id,
+          });
+        })
+        .catch(function (error) {
+          console.error(error);
+          if (error.code === '23505') {
+            res.status(400).json({
+              message: `Movie with title "${newMovie.title}" already exists`,
+            });
+          } else {
+            res.status(500).json({ message: 'Error while creating the movie' });
+          }
+        });
+      */
+      res.json({
+        message: 'Single TMDB movie fetched',
+        results: axiosres.data.results,
+      });
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).json({
+        message: 'Error while fetching the movie',
+        error: err.message,
+      });
+    });
 });
 
 router.get('/', function (req, res) {
@@ -41,10 +95,10 @@ router.get('/', function (req, res) {
     .then(function (movielist) {
       res.json({ movies: movielist });
       console.log(movielist);
+    })
+    .catch(function () {
+      res.status(500).json({ message: 'Error while fetching the Movie' });
     });
-  //  .catch(function () {
-  //    res.status(500).json({ message: 'Error while fetching the Movie' });
-  //  });
 });
 
 router.post('/new', function (req, res) {
@@ -60,7 +114,6 @@ router.post('/new', function (req, res) {
   });
   movieRepository
     .insert(newMovie)
-    //.save(newMovie);
     .then(function (savedMovie) {
       res.status(201).json({
         message: 'Movie successfully created',
