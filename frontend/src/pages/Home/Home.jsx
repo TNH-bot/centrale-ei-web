2f00152072be4bb99ec2e1ea8c76bb706405b322
Hiba Lahmadi <hiba.lahmadi@student-cs.fr>
2025-06-06 08:54:53 +0200

    coucou

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,8 +1,8 @@
 import './Home.css';
 import { useEffect, useState } from 'react';
-import axios from 'axios';
 import logo from './logo.svg';
 import Movie from '../../components/Movie/Movie';
+import { api } from '../../api';
 
 const DEFAULT_FORM_VALUES = {
   searchMovie: '',
@@ -11,37 +11,20 @@ const DEFAULT_FORM_VALUES = {
 function Home() {
   const [formValues, setFormValues] = useState(DEFAULT_FORM_VALUES);
   const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    api
+      .get('/recommendations/1')
+      .then((res) => setMovies(res.data))
+      .catch((err) => console.error(err));
+  }, []);
+
   const listmovies = movies
-    .filter((movie) => movie.title.includes(formValues.searchMovie))
+    .filter((movie) =>
+      movie.title.toLowerCase().includes(formValues.searchMovie.toLowerCase())
+    )
     .map((movie) => <Movie prop={movie} />);
   const isEmptyMovies = listmovies.length === 0;
-
-  function useFetchmovies() {
-    useEffect(() => {
-      const options = {
-        method: 'GET',
-        url: 'https://api.themoviedb.org/3/movie/top_rated',
-        params: { language: 'en-US', page: '1' },
-        headers: {
-          accept: 'application/json',
-          Authorization:
-            'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxZjlmNjAwMzY4MzMzODNkNGIwYjNhNzJiODA3MzdjNCIsInN1YiI6IjY0NzA5YmE4YzVhZGE1MDBkZWU2ZTMxMiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Em7Y9fSW94J91rbuKFjDWxmpWaQzTitxRKNdQ5Lh2Eo',
-        },
-      };
-
-      axios
-        .request(options)
-        .then((res) => {
-          console.log(res.data.results);
-          setMovies(res.data.results);
-        })
-        .catch((err) => console.error(err));
-      console.log('HelloWorld');
-    }, []);
-  }
-  // ...
-
-  useFetchmovies();
 
   return (
     <div className="App">
@@ -49,7 +32,7 @@ function Home() {
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Movielist</h1>
         <input
-          placeholder="Speed"
+          placeholder="Search..."
           value={formValues.searchMovie}
           onChange={(event) =>
             setFormValues({ ...formValues, searchMovie: event.target.value })
@@ -60,20 +43,8 @@ function Home() {
         {isEmptyMovies ? (
           <p className="App-intro">No movies found</p>
         ) : (
-          <div class="container">{listmovies}</div>
+          <div className="container">{listmovies}</div>
         )}
-        {/*<p>
-          Edit <code>src/App.jsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://react.dev"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        */}
       </header>
     </div>
   );
